ae1cd7dd6d9ced8e1a2695030b912f35ad07ad1c
metalfrenzy747 <allenjeremy747@gmail.com>
2023-12-23 17:05:42 -0500

    success

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+const guests=["Guadalupe","Ollie", "Aki"];
+function writeCards(names) {
+    let messages = []
+    for (let i = 0; i < names.length; i++) {
+      messages.push(`Thank you, ${guests[i]}, for the wonderful surprise gift!`)
+    }
+    return messages;
+}
+function countDown(startingNumber){
+    if (startingNumber< 0 || !
+        Number.isInteger(startingNumber)) {
+            console.log("Positive numbers only");
+            return;
+        }
+    for (let i=startingNumber; i>=0; i--){
+        console.log(i);
+    }
+}
+    
---
+++ newFile.js
@@ -0,0 +1,5 @@
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    function countDown();
+    let newYear = 10;
+}
